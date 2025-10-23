8b8aa219cd786f4647287ea1129a051e160f5963
TingYi Wu <tingyi.wu1@gmail.com>
2025-10-23 17:26:07 -0400

    tmux

---
+++ tmux/.config/scripts/tmux-session-dispensary.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SEARCH_DIRS=(
+    "$HOME/Projects"
+)
+STATIC_DIRS=(
+    "$HOME/.config"
+)
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    selected=$(
+        {
+            find "${SEARCH_DIRS[@]}" -type d -maxdepth 1 -mindepth 1
+            printf '%s\n' "${STATIC_DIRS[@]}"
+        } | sed "s|^$HOME/||" | fzf
+    )
+
+    [[ $selected ]] && selected="$HOME/$selected"
+fi
+
+[[ ! $selected ]] && exit 0
+
+selected_name=$(basename "$selected" | tr . _)
+
+if ! tmux has-session -t "$selected_name"; then
+    tmux new-session -ds "$selected_name" -c "$selected"
+    tmux select-window -t "$selected_name:1"
+fi
+
+tmux switch-client -t "$selected_name"
---
+++ tmux/.config/scripts/tmux-status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pane_id=$1
+
+# pane_pid=$(tmux list-panes -F "#{pane_pid}")
+
+# echo "$pane_pid"
+echo "$pane_id"
+
+# python_env=$(which python | sed -n 's|.*/\([^/]*\)/bin/python|(\1)|p')
+#
+# echo "$python_env"
+
